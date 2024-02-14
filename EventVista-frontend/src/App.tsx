428a69dda7d0f90e7822893e8fa9dcbfe9dfbbc3
AnshBhimani <anshbhimani2003@gmail.com>
2024-02-14 22:27:40 +0530

    Changes in file structure

--- EventVista-frontend/src/App.tsx
+++ EventVista-frontend/src/App.tsx
@@ -2,15 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar1 from './Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar1 />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      </>
+    </>
   )
 }
 
